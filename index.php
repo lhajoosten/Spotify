615d0e59b418028a6add931c703485c914ca7d36
Luc Joosten <lhajoost@avans.nl>
2020-02-27 00:16:23 +0100

    made the center component of the home page

--- index.php
+++ index.php
@@ -27,7 +27,36 @@ if (isset($_SESSION['userLoggedIn'])) {
 
         </div>
         <div id="nowPlayingCenter">
+            <div class="content playerControls">
+                <div class="buttons">
+                    <button class="controlButton" title="Shuffle button">
+                        <img src="assets/img/icons/shuffle.png" alt="shuffle"></button>
+                    <button class="controlButton" title="Back button">
+                        <img src="assets/img/icons/back.png" alt="back">
+                    </button>
+                    <button class="controlButton" title="Play button">
+                        <img style="height: 32px; width: 32px;" src="assets/img/icons/play.png" alt="play">
+                    </button>
+                    <button style="height: 32px; width: 32px; display: none" class="controlButton" title="Pause button">
+                        <img src="assets/img/icons/play.png" alt="play"></button>
+                    <button class="controlButton" title="Next button">
+                        <img src="assets/img/icons/next.png" alt="next">
+                    </button>
+                    <button class="controlButton" title="Repeat button">
+                        <img src="assets/img/icons/repeat.png" alt="repeat">
+                    </button>
 
+                </div>
+                <div class="playbackBar">
+                    <span class="progressTime current">0.00</span>
+                    <div class="progressBar">
+                        <div class="progressBarBg">
+                            <div class="progress"></div>
+                        </div>
+                    </div>
+                    <span class="progressTime remaining">0.00</span>
+                </div>
+            </div>
         </div>
         <div id="nowPlayingRight">
 
