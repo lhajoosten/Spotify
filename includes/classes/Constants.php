8aa0ffc6f5a5004d942a62e27a7ea1071f41dac4
Luc Joosten <lhajoost@avans.nl>
2020-02-26 14:52:57 +0100

    created new constants class

---
+++ includes/classes/Constants.php
@@ -0,0 +1,15 @@
+<?php
+
+
+class Constants
+{
+    public static $usernameLengthError = "Your username must be between 25 & 5 characters";
+    public static $firstNameLengthError = "Your first name must be between 25 & 2 characters";
+    public static $lastNameLengthError = "Your last name must be between 25 & 2 characters";
+    public static $emailDoNotMatchError = "Your emails don't match";
+    public static $emailNotValidError = "Your email is invalid";
+    public static $passwordsDoNotMatchError = "Your passwords don't match";
+    public static $passwordNotValidError = "Your password can only contain numbers and letters";
+    public static $passwordLengthError = "Your password must be between 30 & 50 characters";
+
+}
