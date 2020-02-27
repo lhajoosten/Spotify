d90100b677d38eb462f8f0422cbd2e0aba41427e
Luc Joosten <lhajoost@avans.nl>
2020-02-27 17:22:30 +0100

    fetching albums from database

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
-<?php
-include("includes/components/header.php");
-?>
+<?php include("includes/components/header.php"); ?>
 
+<h1 class="pageHeadingBig">You Might Also Like</h1>
 
+<div class="gridViewContainer">
+    <?php
+    $albumQuery = mysqli_query($con, "SELECT * FROM Albums");
+        while ($row = mysqli_fetch_array($albumQuery)) {
+            echo $row['title'] . "<br>";
+        }
+    ?>
+</div>
 
-<?php
-include("includes/components/footer.php");
-?>
+<?php include("includes/components/footer.php"); ?>
 
 
 
